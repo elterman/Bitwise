5ecf4bb1b4dc49cd50cfe2f49b6ddf5e68d32f0c
Arkady Elterman <elterman@gmail.com>
2025-08-23 14:54:47 -0700

    .

--- src/Instructions.svelte
+++ src/Instructions.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <div class="instr">
-    Combine the two inputs from the queue by applying an available operator. The selected operator will be unavailable on the next turn.
+    Combine the two inputs from the queue by applying an available operator above. The selected operator will become unavailable on the next turn.
 </div>
 
 <style>
@@ -10,8 +10,8 @@
         grid-area: 5/1;
         place-self: center;
         display: grid;
-        width: 90%;
         font-family: UI;
+        font-size: 15.6px;
         filter: drop-shadow(0 0 3px #00000080);
         color: var(--blue);
     }
